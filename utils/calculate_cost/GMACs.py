7fa4fb475f189d0ed0d91f5fb5d16f7f25cfc15a
MydasTouch <159111376+MydasTouch@users.noreply.github.com>
2024-11-11 21:30:52 +0900

    Update GMACs.py

--- utils/calculate_cost/GMACs.py
+++ utils/calculate_cost/GMACs.py
@@ -1,7 +1,12 @@
 # 这个计算方法是基于Asteroid，一般是在测试（reference）时候（eval.py）中进行计算。
 
-
 # pip install ptflops
+
+# model_path = os.path.join(conf["exp_dir"], "best_model.pth")
+# model = real_SepFormer.from_pretrained(model_path)
+# if conf["use_gpu"]:
+#     model.cuda()
+# model_device = next(model.parameters()).device
 from ptflops import get_model_complexity_info
 macs, params = get_model_complexity_info(model, (1, 24000), as_strings=True,
                                         print_per_layer_stat=True, verbose=True)
